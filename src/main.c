31139dc25e6f190de0465eb74ad073ce71200a19
CMDR-Piboy314 <92357316+CMDR-Piboy314@users.noreply.github.com>
2022-06-03 12:41:15 -0400

    Comment out Variables with example shaders (uncomment only for debug)

--- src/main.c
+++ src/main.c
@@ -29,28 +29,28 @@ const char *vertexShaderSource;
 
 const char *fragmentShaderSource;
 
-const char *vertexShaderSource =
-	"#version 330 core\n"
+// const char *vertexShaderSource =
+// 	"#version 330 core\n"
 
-    "layout (location = 0) in vec3 aPos;\n"
-    "layout (location = 1) in vec3 aColor;\n"
+//     "layout (location = 0) in vec3 aPos;\n"
+//     "layout (location = 1) in vec3 aColor;\n"
 
-    "out vec3 ourColor;\n"
+//     "out vec3 ourColor;\n"
 
-    "void main() {\n"
-    "   gl_Position = vec4(aPos, 1.0);\n"
-    "   ourColor = aColor;\n"
-    "}\0";
+//     "void main() {\n"
+//     "   gl_Position = vec4(aPos, 1.0);\n"
+//     "   ourColor = aColor;\n"
+//     "}\0";
 
-const char *fragmentShaderSource = 
-	"#version 330 core\n"
+// const char *fragmentShaderSource = 
+// 	"#version 330 core\n"
 
-    "out vec4 FragColor;\n"
-    "in vec3 ourColor;\n"
+//     "out vec4 FragColor;\n"
+//     "in vec3 ourColor;\n"
 
-    "void main() {\n"
-    "   FragColor = vec4(ourColor, 1.0f);\n"
-    "}\n\0";
+//     "void main() {\n"
+//     "   FragColor = vec4(ourColor, 1.0f);\n"
+//     "}\n\0";
 
 // Define functions
 
